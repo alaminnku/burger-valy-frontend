0a829f9267cd49b10519f98c55f5a176bacab5c8
Alamin Shaikh <alaminshaikh@Alamins-MacBook-Pro.local>
2021-09-14 13:28:02 +0600

    Api URL updated

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:1337";
+export const API_URL = "https://burger-valley.herokuapp.com";
 export const NEXT_URL = "http://localhost:3000/api";
 
 // "https://burger-valley.herokuapp.com";
