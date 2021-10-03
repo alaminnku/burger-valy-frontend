543310f66d0f2348b5661bda36ce1cd2008b4bc6
Alamin Shaikh <alaminshaikh@Alamins-MacBook-Pro.local>
2021-10-03 10:03:02 +0600

    API URL updated

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ export const API_URL = "https://burger-valley.herokuapp.com";
 export const NEXT_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000/api"
-    : "https://burgervalley.vercel.app/api";
+    : "https://burgervaly.com/api";
 
 // "https://burger-valley.herokuapp.com";
 // "https://burgervalley.vercel.app/api";
