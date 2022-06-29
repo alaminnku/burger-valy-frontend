c00345c513471b13190b5e7dcd1ec9834a5c161e
Alamin Shaikh <alaminshaikh@Alamins-MacBook-Pro.local>
2022-06-29 13:12:30 +0600

    NEXT URL updated

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 export const API_URL = "https://burger-valley.herokuapp.com";
 
-export const NEXT_URL = "http://localhost:3000/api";
+export const NEXT_URL = "https://www.burgervaly.com/api";
 
 // "https://burger-valley.herokuapp.com";
 // "https://burgervalley.vercel.app/api";
